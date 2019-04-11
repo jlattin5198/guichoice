cb83cfd0ecd984bb96b78bd877d455c3e9a0e6ef
jlattin5198 <jlattin5198@student.rcschools.net>
2019-04-11 14:58:52 -0500

    phase 1: ui layout (X) phase 2: moving snake (X) phase 3: dot collection/randomization (_)

--- snake.py
+++ snake.py
@@ -6,6 +6,8 @@ def main():
     root = t.Tk()
     sgame = Game(root)
     root.mainloop()
+
+
 class Game:
     def __init__(self, parent):
         upper = t.Frame(parent)
@@ -17,10 +19,43 @@ class Game:
         upper_scr.pack()
         lower.pack()
 
-        title = t.Label(upper, text="Snake")
+        title = t.Label(upper, text="Snake - Use arrow keys")
         title.pack()
 
-        
+        s_btn = t.Button(upper_btn, text="Start")
+        s_btn.pack(side="left")
+        r_btn = t.Button(upper_btn, text="Retry", state="disabled")
+        r_btn.pack(side="right")
+
+        self.score = 0
+        self.scr_lab = t.Label(upper_scr, text="Score: " + str(self.score))
+        self.scr_lab.pack()
+
+        self.gamewin = t.Canvas(lower, height="350", width="350", bg="white", borderwidth="7", relief="ridge")
+        self.gamewin.pack()
+        self.gamewin.focus_force()
+        self.gamewin.bind("<Left>", self.slither)
+        self.gamewin.bind("<Right>", self.slither)
+        self.gamewin.bind("<Up>", self.slither)
+        self.gamewin.bind("<Down>", self.slither)
+
+        self.snake = self.gamewin.create_rectangle(172, 172, 192, 192, tags="hiss", fill="blue")
+
+        self.dot = self.gamewin.create_rectangle(177, 177, 187, 187)
+
+    def slither(self, event):
+        dir = str(event.keysym)
+        dx = 0
+        dy = 0
+        if dir == "Left":
+            dx = -20
+        elif dir == "Right":
+            dx = 20
+        elif dir == "Up":
+            dy = -20
+        else:
+            dy = 20
+        self.gamewin.move(self.snake, dx, dy)
 
 
 main()
